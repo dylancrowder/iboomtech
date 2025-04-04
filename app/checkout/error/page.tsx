370a38d3d47f2e914ce3c3c0332b51e7ed3019f1
dylan <devdylancrowder@outlook.com>
2025-04-04 10:53:14 -0300

    suspence

--- app/checkout/error/page.tsx
+++ app/checkout/error/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import Link from "next/link";
+import ErrorCompo from "@/secciones/checkout/ErrorCompon";
+import { Suspense } from "react";
 
 export default function ErrorPago() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold text-red-600">Pago fallido ❌</h1>
-      <p>Hubo un problema al procesar tu pago. Intenta de nuevo.</p>
-      <Link href="/" className="mt-4 text-blue-500">Volver a la tienda</Link>
-    </div>
+    <Suspense fallback={<p>Cargando...</p>}>
+      <ErrorCompo />
+    </Suspense>
   );
 }

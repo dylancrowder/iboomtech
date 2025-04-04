5380c1a1697391ca1e737151cd5f1d581ce8803b
dylan <devdylancrowder@outlook.com>
2025-04-04 18:11:19 -0300

    sd

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -23,26 +23,28 @@ import { useCarritoStore } from "@/zustand/carritoStore";
 import logo from "../../assets/imagenes/logotipo/LOGO-negro-iboom.png";
 // Schema for validation
 const paymentSchema = z.object({
-  nombre: z.string().min(2, "El nombre debe tener al menos 2 caracteres"),
-  apellido: z.string().min(2, "El apellido debe tener al menos 2 caracteres"),
+  nombre: z.string().min(4, "El nombre debe tener al menos 4 caracteres"),
+  apellido: z.string().min(4, "El apellido debe tener al menos 4 caracteres"),
   email: z.string().email("Debe ser un correo válido"),
   direccion: z
     .string()
     .min(5, "La dirección debe tener al menos 5 caracteres")
     .optional(),
-  ciudad: z.string().min(2, "Debe especificar una ciudad").optional(),
+  ciudad: z.string().min(2, "Debe especificar una ciudad"),
   codigo_postal: z
     .string()
     .min(2, "Debe especificar una codigo postal")
     .optional(),
+
   apartamento_opcional: z.string().optional(),
-  provincia: z.string().min(2, "Debe especificar una provincia").optional(),
+
+  provincia: z.string().min(2, "Debe especificar una provincia"),
   telefono: z
     .string()
     .min(10, "El teléfono debe tener al menos 10 caracteres")
     .optional(),
-  dni: z.string().min(2, "Debe especificar una dni").optional(),
-  envio: z.enum(["domicilio", "retiro"]).default("domicilio"),
+  dni: z.string().min(5, "Debe especificar una dni"),
+  envio: z.enum(["domicilio", "retiro"]),
   metodoPago: z.enum(["mercadopago", "transferencia"]),
 });
 
